e887f7eba5bba37e023ae0947880bac3ac11a852
Tait Weinert <tait.weinert@outlook.com>
2024-02-26 14:27:39 +1030

    added schemas

---
+++ models/message.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  title: { type: String, required: true },
+  timestamp: { type: Date, required: true, default: Date.now },
+  text: { type: String },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+// Export model
+module.exports = mongoose.model("Message", MessageSchema);
---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  membership_status: {
+    type: String,
+    required: true,
+    enum: ["Active", "Inactive" ],
+    default: "Active",
+  },
+});
+
+// Virtual for author's full name
+UserSchema.virtual("full_name").get(function () {
+  // To avoid errors in cases where an author does not have either a family name or first name
+  // We want to make sure we handle the exception by returning an empty string for that case
+  let fullname = "";
+  if (this.first_name && this.family_name) {
+    fullname = `${this.family_name}, ${this.first_name}`;
+  }
+
+  return fullname;
+});
+
+// Export model
+module.exports = mongoose.model("User", UserSchema);
