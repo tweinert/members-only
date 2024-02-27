16aafdab5628519bf741afe0fefd082e5f86e903
Tait Weinert <tait.weinert@outlook.com>
2024-02-27 17:00:09 +1030

    added sign up form

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,7 +7,7 @@ exports.index = asyncHandler(async (req, res, next) => {
 });
 
 exports.sign_up_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: sign up GET");
+  res.render("sign_up_form", { title: "Sign Up" });
 });
 
 exports.sign_up_post = asyncHandler(async (req, res, next) => {
