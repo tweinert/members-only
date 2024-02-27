ddf9441379422610c58db5cf7d348c67a42effa1
Tait Weinert <tait.weinert@outlook.com>
2024-02-27 17:28:35 +1030

    added sign up post

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,8 @@
 const User = require("../models/user");
 const Message = require("../models/message");
+const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
 
 exports.index = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Site Home Page");
@@ -10,9 +12,42 @@ exports.sign_up_get = asyncHandler(async (req, res, next) => {
   res.render("sign_up_form", { title: "Sign Up" });
 });
 
-exports.sign_up_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: sign up POST");
-});
+exports.sign_up_post = [
+  // validate and sanitize inputs 
+  body("first_name", "Name must not be empty")
+    .trim()
+    .isLength({ min: 1})
+    .escape(),
+  body("family_name", "Name must not be empty")
+    .trim()
+    .isLength({ min: 1})
+    .escape(),
+  body("email", "Please enter valid email")
+    .trim()
+    .isEmail()
+    .escape(),
+  body("password", "Password must be a minimum of 6 characters long")
+    .trim()
+    .isLength({ min: 6 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    bcrypt.hash(req.body.password, 10, async(err, hashedPassword) => {
+      if (err) {
+        return next(err);
+      }
+      const user = new User({
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        email: req.body.email,
+        password: hashedPassword,
+        membership_status: "Inactive"
+      });
+      const result = await user.save();
+      res.redirect("/");
+    });
+  }),
+];
 
 exports.log_in_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: log in GET");
